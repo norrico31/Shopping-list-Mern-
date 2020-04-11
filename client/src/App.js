82faf5f3f56f5383ab8d33610995a0b6225876c9
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-11 20:15:07 +0800

    added ShoppingList.js and add transition fade effect using css in index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import ShoppingList from './components/ShoppingList';
 
 import AppNavbar from './components/AppNavbar';
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
           <AppNavbar />
+          <ShoppingList />
       </div>
     );
   }

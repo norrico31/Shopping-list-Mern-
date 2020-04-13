3c479b3ac1012ef036c082d1036a7c23e78e6a2a
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-13 15:20:01 +0800

    try some changes in server.js

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ app.use('/api/items', items);
 
 
 // Serve static assets if in production
-if(process.env.NODE_ENV ==='production') {
-    // set static folder
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`[server.js]: running on PORT ${PORT}`.yellow));

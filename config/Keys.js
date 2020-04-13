762d603df8af59284cf5247569397139aa49d7f8
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-13 18:43:52 +0800

    make it better

--- config/Keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://norrico31:Levi313131@mongoose-express-lhkys.mongodb.net/expense-tracker?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -2,39 +2,32 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const colors = require('colors');
-const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config({ path: './config/config.env' });
 
 const items = require('./routes/api/items');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
 // Body-Parser middleware
 app.use(bodyParser.json());
-app.use(cors());
-
-// DB Config
-const db = require('./config/Keys').MongoURI;
 
 //connect to DB
-mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-.then(() => console.log(`MongoDB Connected!`.yellow.bold))
-.catch((err) => console.log({ err }).red.bold);
+mongoose.connect(process.env.MongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log(`MongoDB Connected!`))
+.catch((err) => console.log({ err }));
 
 // ROUTES
 app.use('/api/items', items);
 
-
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+  });
+}
 
-app.listen(PORT, () => console.log(`[server.js]: running on PORT ${PORT}`.yellow));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`[server.js]: running on PORT ${PORT}`));
